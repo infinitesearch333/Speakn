a65269c829f0ac61d2e33337d46f27d34d8ffab3
Sergio Rosendo <srosendo369@hotmail.com>
2019-03-01 11:39:39 -0800

    Base project with Dropbox SDK installed

---
+++ Speakn/Speakn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Speakn
+//
+//  Created by Sergio Rosendo on 3/1/19.
+//  Copyright © 2019 Sergio Rosendo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
